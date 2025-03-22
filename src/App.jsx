0edabed6956bebc0de7a42c2c9a62620097b2859
jogendra05 <jogendrasuthar03@gmail.com>
2025-03-22 22:23:12 +0530

    Improve Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./Components/Home";
 import Header from "./Components/Header";
+import ScrollReveal from 'scrollreveal';
+
 
 function App() {
+  const [activeSection, setActiveSection] = useState('home');
+  useEffect(() => {
+    const sr = ScrollReveal({
+      origin: 'top',
+      distance: '60px',
+      duration: 2000,
+      delay: 200,
+    });
+
+    sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+    sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 400 });
+    sr.reveal('.home__social-icon', { interval: 200 });
+    sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
+
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section[id]');
+      sections.forEach(section => {
+        const top = window.scrollY;
+        const offset = section.offsetTop - 58;
+        const height = section.offsetHeight;
+        const id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+          setActiveSection(id);
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
     <>
-      <Header/>
+      <Header activeSection={activeSection} />
       <Home/>
     </>
   );
